20b6702faa2ec749258ec01c4677c081a80518a9
Ekaterina <militereva@mail.ru>
2024-05-07 12:53:03 +0300

    Add LoginPage and NotFoundPage components

---
+++ frontend/src/components/NotFoundPage.jsx
@@ -0,0 +1,26 @@
+const NotFoundPage = () => {
+  return (
+    <div className="h-100">
+      <div className="h-100" id="chat">
+        <div className="d-flex flex-column h-100">
+          <nav className="shadow-sm navbar navbar-expand-lg navbar-light bg-white">
+            <div className="container">
+              <a className="navbar-brand" href="/">
+                Hexlet Chat
+              </a>
+            </div>
+          </nav>
+          <div className="text-center">
+            <h1 className="h4 text-muted">Страница не найдена</h1>
+            <p className="text-muted">
+              Но вы можете перейти <a href="/">на главную страницу</a>
+            </p>
+          </div>
+        </div>
+        <div className="Toastify"></div>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
