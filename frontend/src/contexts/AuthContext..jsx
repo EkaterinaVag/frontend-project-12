85c593b6f54cce12d04c59c5a6020753f7516553
Ekaterina <militereva@mail.ru>
2024-06-27 15:52:10 +0300

    Add AuthContext.jsx

---
+++ frontend/src/contexts/AuthContext..jsx
@@ -0,0 +1,19 @@
+import { createContext, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const logIn = () => setLoggedIn(true);
+  const logOut = () => {
+    localStorage.removeItem("userId");
+    setLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ loggedIn, logIn, logOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
