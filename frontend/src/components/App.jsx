a7022e567319a58ea40aaf9706bd040e5fcd635b
Ekaterina <militereva@mail.ru>
2024-06-27 15:51:22 +0300

    Move App.jsx to components folder

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from "./LoginPage";
+import NotFoundPage from "./NotFoundPage";
+import { AuthProvider } from "../contexts/AuthContext.";
+import { Navbar, Container } from "react-bootstrap";
+
+const App = () => {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <div className="d-flex flex-column h-100">
+          <Navbar expand="lg" className="shadow-sm navbar-light bg-white">
+            <Container>
+              <Navbar.Brand href="#/">Hexlet Chat</Navbar.Brand>
+            </Container>
+          </Navbar>
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+};
+
+export default App;
