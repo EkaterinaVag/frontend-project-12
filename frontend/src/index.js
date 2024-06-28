e97db305d7221982e6c0006afdd4456f80c09c97
Ekaterina <militereva@mail.ru>
2024-06-28 12:26:28 +0300

    Add Provider redux component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from 'react-redux';
+
+import { store } from './store';
 import App from './components/App';
 
 const mountNode = document.getElementById('root');
 const root = ReactDOM.createRoot(mountNode);
-root.render(<App />);
+root.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  );
