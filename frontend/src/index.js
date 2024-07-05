902d0cb9a88115b064ea61ee5ec347096dcdffce
Ekaterina <militereva@mail.ru>
2024-07-05 15:21:31 +0300

    Add BrowserRouter

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from 'react-redux';
+import ReactDOMClient from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
-import { store } from './store';
-import App from './components/App';
+import { store } from "./store";
+import App from "./components/App";
 
-const mountNode = document.getElementById('root');
-const root = ReactDOM.createRoot(mountNode);
+const mountNode = document.getElementById("root");
+const root = ReactDOMClient.createRoot(mountNode);
 root.render(
+  <BrowserRouter>
     <Provider store={store}>
       <App />
-    </Provider>,
-  );
+    </Provider>
+  </BrowserRouter>
+);
