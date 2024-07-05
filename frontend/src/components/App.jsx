41a139ed9fdd880cdbe4e36ddda998533b5b5f4c
Ekaterina <militereva@mail.ru>
2024-07-05 15:17:17 +0300

    Add logout btn

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,29 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import LoginPage from "./LoginPage";
 import NotFoundPage from "./NotFoundPage";
 import ChatPage from "./ChatPage";
-import { AuthProvider } from "../contexts/AuthContext.";
-import { Navbar, Container } from "react-bootstrap";
+import { AuthProvider, AuthContext } from "../contexts/AuthContext";
+import { Navbar, Container, Button } from "react-bootstrap";
 
 const App = () => {
+  const { loggedIn, logOut } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logOut();
+    navigate("/login");
+  };
+
   return (
     <AuthProvider>
-      <BrowserRouter>
-        <div className="d-flex flex-column h-100">
-          <Navbar expand="lg" className="shadow-sm navbar-light bg-white">
-            <Container>
-              <Navbar.Brand href="#/">Hexlet Chat</Navbar.Brand>
-            </Container>
-          </Navbar>
-          <Routes>
-            <Route path="/" element={<ChatPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
+      <div className="d-flex flex-column h-100">
+        <Navbar expand="lg" className="shadow-sm navbar-light bg-white">
+          <Container>
+            <Navbar.Brand href="#/">Hexlet Chat</Navbar.Brand>
+            {loggedIn && (
+              <Button variant="primary" onClick={handleLogout}>
+                Выйти
+              </Button>
+            )}
+          </Container>
+        </Navbar>
+        <Routes>
+          <Route path="/" element={<ChatPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
     </AuthProvider>
   );
 };
