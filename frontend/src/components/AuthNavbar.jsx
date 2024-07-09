91466146dce237d26511a3e3f241476eed06d148
Ekaterina <militereva@mail.ru>
2024-07-09 17:27:06 +0300

    Add AuthNavbar

---
+++ frontend/src/components/AuthNavbar.jsx
@@ -0,0 +1,30 @@
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../authSlice";
+import { Navbar, Container, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
+const AuthNavbar = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate("/login");
+  };
+
+  return (
+    <Navbar expand="lg" className="shadow-sm navbar-light bg-white">
+      <Container>
+        <Navbar.Brand href="#/">Hexlet Chat</Navbar.Brand>
+        {isAuthenticated && (
+          <Button variant="primary" onClick={handleLogout}>
+            Выйти
+          </Button>
+        )}
+      </Container>
+    </Navbar>
+  );
+};
+
+export default AuthNavbar;
