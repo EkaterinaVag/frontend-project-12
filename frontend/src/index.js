459479d0f08eabe8401f83b746221add68314dea
Ekaterina <militereva@mail.ru>
2024-07-09 17:24:54 +0300

    Move BrowserRouter to App component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOMClient from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
 
 import { store } from "./store";
 import App from "./components/App";
@@ -9,9 +8,7 @@ import App from "./components/App";
 const mountNode = document.getElementById("root");
 const root = ReactDOMClient.createRoot(mountNode);
 root.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
