b9b2c8b9dcb6e1e8d057c420e28c13f007966eba
Ekaterina <militereva@mail.ru>
2024-07-09 17:19:45 +0300

    Move Navbar to AuthNavbar component

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,41 +1,23 @@
-import React, { useContext } from "react";
+import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./LoginPage";
 import NotFoundPage from "./NotFoundPage";
 import ChatPage from "./ChatPage";
-import { AuthProvider, AuthContext } from "../contexts/AuthContext";
-import { Navbar, Container, Button } from "react-bootstrap";
+import AuthNavbar from "./Navbar";
 
 const App = () => {
-  const { loggedIn, logOut } = useContext(AuthContext);
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    logOut();
-    navigate("/login");
-  };
-
   return (
-    <AuthProvider>
+    <BrowserRouter>
       <div className="d-flex flex-column h-100">
-        <Navbar expand="lg" className="shadow-sm navbar-light bg-white">
-          <Container>
-            <Navbar.Brand href="#/">Hexlet Chat</Navbar.Brand>
-            {loggedIn && (
-              <Button variant="primary" onClick={handleLogout}>
-                Выйти
-              </Button>
-            )}
-          </Container>
-        </Navbar>
+        <AuthNavbar />
         <Routes>
           <Route path="/" element={<ChatPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </div>
-    </AuthProvider>
+    </BrowserRouter>
   );
 };
 
