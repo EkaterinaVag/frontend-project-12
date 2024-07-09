a25912606cb3209f490fc4d78cd1c10292c4aafa
Ekaterina <militereva@mail.ru>
2024-07-09 17:25:45 +0300

    Add authReducer and modalReducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,15 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { setupListeners } from '@reduxjs/toolkit/query';
+import { setupListeners } from "@reduxjs/toolkit/query";
 import { chatApi } from "./api/chatApi";
+import authReducer from "./authSlice";
+import modalReducer from "./modalSlice";
 
 export const store = configureStore({
   reducer: {
     [chatApi.reducerPath]: chatApi.reducer,
+    auth: authReducer,
+    modal: modalReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(
-      chatApi.middleware,
-    ),
+    getDefaultMiddleware().concat(chatApi.middleware),
 });
 
 setupListeners(store.dispatch);
