403a874c46835ca79f48230742ff523b3fa7b3b0
Ekaterina <militereva@mail.ru>
2024-07-22 16:12:25 +0300

    Add locales folder

---
+++ frontend/src/locales/ru.js
@@ -0,0 +1,53 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  translation: {
+    modals: {
+      addChannel: "Добавить канал",
+      delete: "Удалить канал",
+      confifm: "Уверены?",
+      rename: "Переименовать канал",
+    },
+    buttons: {
+      cancel: "Отменить",
+      add: "Добавить",
+      send: "Отправить",
+      remove: "Удалить",
+      rename: "Переименовать",
+      logout: "Выйти",
+      login: "Войти",
+    },
+    validate: {
+      min: "Не менее 6  символов",
+      max: "От 3 до 20 символов",
+      required: "Обязательное поле",
+      confPass: "Пароли должны совпадать",
+      unique: "Должно быть уникальным",
+    },
+    loginPage: {
+      username: "Ваш ник",
+      password: "Пароль",
+      error: "Неверные имя пользователя или пароль",
+      exist: "Нет аккаунта?",
+      registration: "Регистрация",
+    },
+    notFoundPage: {
+      noExist: "Страница не найдена",
+      link: "Но вы можете перейти",
+      main: "на главную страницу",
+    },
+    signupPage: {
+      username: "Имя пользователя",
+      password: "Пароль",
+      confirmPassword: "Подтвердите пароль",
+      registration: "Зарегистрироваться",
+    },
+    messages: {
+      loading: "Загрузка сообщений...",
+      key_one: "{{count}} сообщение",
+      key_few: "{{count}} сообщения",
+      key_many: "{{count}} сообщений",
+    },
+    channels: "Каналы",
+    dropdown: "Управление каналом",
+  },
+};
