01cad086ba2dfa9c2ea8b5eaa03bf1d32823368c
Ekaterina <militereva@mail.ru>
2024-07-25 14:19:22 +0300

    Fix app init logic

--- frontend/src/init.js
+++ frontend/src/init.js
@@ -1,15 +1,28 @@
 import i18next from "i18next";
+import { initReactI18next, I18nextProvider } from "react-i18next";
+import { Provider } from "react-redux";
+
 import resources from "./locales/index";
+import { store } from "./store";
+import App from "./components/App";
 
 const init = async () => {
   const defaultLang = "ru";
 
   const i18n = i18next.createInstance();
-  await i18n.init({
+  await i18n.use(initReactI18next).init({
     lng: defaultLang,
-    debug: false,
+    debug: true,
     resources,
   });
+
+  return (
+    <I18nextProvider i18n={i18n}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </I18nextProvider>
+  );
 };
 
 export default init;
