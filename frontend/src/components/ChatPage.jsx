605f228ceddb78a6cc2e844e7aa0d7f30f4f4059
Ekaterina <militereva@mail.ru>
2024-07-25 14:24:38 +0300

    Add useTranslation i18n for texts

--- frontend/src/components/ChatPage.jsx
+++ frontend/src/components/ChatPage.jsx
@@ -1,12 +1,15 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
+import { useTranslation } from "react-i18next";
+
 import { openAddChannelModal } from "../slices/modalSlice";
 import { setupSocket } from "../api/chatApi";
 import Messages from "./Messages";
 import ChannelList from "./ChannelsList";
 
 const ChatPage = () => {
+  const { t } = useTranslation();
   const dispatch = useDispatch();
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
   const navigate = useNavigate();
@@ -27,7 +30,7 @@ const ChatPage = () => {
       <div className="row h-100 bg-white flex-md-row">
         <div className="col-4 col-md-2 border-end px-0 bg-light flex-column h-100 d-flex">
           <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
-            <b>Каналы</b>
+            <b>{t('channels')}</b>
             <button
               type="button"
               className="p-0 text-primary btn btn-group-vertical"
