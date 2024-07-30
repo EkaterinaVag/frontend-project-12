fb5732e236407385e5ed9df415f2e86c2a8350c2
Ekaterina <militereva@mail.ru>
2024-07-30 19:00:15 +0300

    Separate modal render to ModalComponent

--- frontend/src/components/modals/RenameChannelModal.jsx
+++ frontend/src/components/modals/RenameChannelModal.jsx
@@ -1,20 +1,19 @@
 import React, { useEffect, useRef } from 'react';
 import { useSelector } from 'react-redux';
-import {
-  Modal, FormGroup, FormControl, Button,
-} from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import { useFormik } from 'formik';
 import { toast } from 'react-toastify';
 import filter from 'leo-profanity';
 
 import channelNameValidate from '../../channelNameValidate';
+import ModalComponent from './ModalComponent';
 import {
   useGetChannelsQuery,
   useRenameChannelMutation,
 } from '../../api/chatApi';
 
 const RenameChannelModal = ({ onHide }) => {
+  const modalName = 'rename';
   const { t } = useTranslation();
 
   const selectedChannel = useSelector((state) => state.modal.selectedChannel);
@@ -46,47 +45,14 @@ const RenameChannelModal = ({ onHide }) => {
   });
 
   return (
-    <Modal show centered>
-      <Modal.Header closeButton onHide={onHide}>
-        <Modal.Title>{t('modals.rename')}</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <form onSubmit={formik.handleSubmit}>
-          <FormGroup>
-            <FormControl
-              required
-              ref={inputRef}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.name}
-              isInvalid={formik.touched.name && formik.errors.name}
-              data-testid="input-name"
-              name="name"
-            />
-            {formik.touched.name && formik.errors.name && (
-              <FormControl.Feedback type="invalid">
-                {formik.errors.name}
-              </FormControl.Feedback>
-            )}
-          </FormGroup>
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'flex-end',
-              gap: '5px',
-              marginTop: '15px',
-            }}
-          >
-            <Button variant="secondary" onClick={onHide}>
-              {t('buttons.cancel')}
-            </Button>
-            <Button variant="primary" type="submit" disabled={isLoading}>
-              {t('buttons.send')}
-            </Button>
-          </div>
-        </form>
-      </Modal.Body>
-    </Modal>
+    <ModalComponent
+      onHide={onHide}
+      formik={formik}
+      modalName={modalName}
+      isLoading={isLoading}
+      t={t}
+      inputRef={inputRef}
+    />
   );
 };
 
