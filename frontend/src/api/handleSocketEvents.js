8dec4ab635ed90709c37278a3701bcf3cc5fb647
Ekaterina <militereva@mail.ru>
2024-08-03 16:15:32 +0300

    Move socket connections to chatApi

--- frontend/src/api/handleSocketEvents.js
+++ frontend/src/api/handleSocketEvents.js
@@ -1,8 +1,5 @@
-import { io } from 'socket.io-client';
-
-const socket = io('http://localhost:3000');
-
 const handleSocketEvents = async (
+  socket,
   eventName,
   updateCachedData,
   cacheDataLoaded,
