1aeff15e1c6e3de26d403874b434448c5580c9a8
Ekaterina <militereva@mail.ru>
2024-08-03 13:40:22 +0300

    Add handleSocketEvents

---
+++ frontend/src/api/handleSocketEvents.js
@@ -0,0 +1,26 @@
+import { io } from 'socket.io-client';
+
+const socket = io();
+
+const handleSocketEvents = async (
+  eventName,
+  updateCachedData,
+  cacheDataLoaded,
+  cacheEntryRemoved,
+) => {
+  try {
+    await cacheDataLoaded;
+    const handleEvent = (payload) => {
+      updateCachedData((draft) => {
+        draft.push(payload);
+      });
+    };
+    socket.on(eventName, handleEvent);
+  } catch (err) {
+    console.error(err);
+  }
+  await cacheEntryRemoved;
+  socket.off(eventName);
+};
+
+export default handleSocketEvents;
