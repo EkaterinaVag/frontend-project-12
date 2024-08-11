2a704517457913bf1d0dd10273e00a146a61bc91
Ekaterina <militereva@mail.ru>
2024-08-11 13:33:29 +0300

    Add scroll to MessageBox

--- frontend/src/components/MessagesBox.jsx
+++ frontend/src/components/MessagesBox.jsx
@@ -1,17 +1,30 @@
+import React, { useEffect, useRef } from 'react';
+
 import Spinner from './Spinner';
 
-const MessagesBox = ({ channelMessages, isLoading }) => (
-  <div id="messages-box" className="chat-messages overflow-auto px-5">
-    {isLoading && <Spinner />}
-    {channelMessages
-      && channelMessages.map((message) => (
+const MessagesBox = ({ channelMessages, isLoading }) => {
+  const messagesRef = useRef(null);
+  useEffect(() => {
+    messagesRef.current.scrollTop = messagesRef.current.scrollHeight;
+  }, [channelMessages]);
+
+  return (
+    <div
+      ref={messagesRef}
+      id="messages-box"
+      className="chat-messages overflow-auto px-5"
+    >
+      {isLoading && <Spinner />}
+      {channelMessages?.map((message) => (
         <div key={message.id}>
           <strong>{message.username}</strong>
           :
+          {' '}
           {message.body}
         </div>
       ))}
-  </div>
-);
+    </div>
+  );
+};
 
 export default MessagesBox;
