eff60f1b3890ab1f33ff1132c7c731a84ba7242e
Ekaterina <militereva@mail.ru>
2024-08-12 20:49:28 +0300

    Add signup page check

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -1,15 +1,20 @@
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const useAuth = () => {
   const navigate = useNavigate();
+  const location = useLocation();
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
 
   useEffect(() => {
     if (isAuthenticated) navigate('/');
-    if (!isAuthenticated) navigate('/login');
-  }, [isAuthenticated, navigate]);
+    if (!isAuthenticated) {
+      if (location.pathname !== '/signup') {
+        navigate('/login');
+      }
+    }
+  }, [isAuthenticated, location.pathname, navigate]);
 };
 
 export default useAuth;
