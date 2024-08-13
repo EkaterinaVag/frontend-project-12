8fadbee8e031691079c1e5805addba60a7ae13da
Ekaterina <militereva@mail.ru>
2024-08-13 10:52:58 +0300

    Refactor useAuth hook

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -2,17 +2,17 @@ import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate, useLocation } from 'react-router-dom';
 
+import { appRoutes } from '../routes';
+
 const useAuth = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
 
   useEffect(() => {
-    if (isAuthenticated) navigate('/');
-    if (!isAuthenticated) {
-      if (location.pathname !== '/signup') {
-        navigate('/login');
-      }
+    if (isAuthenticated) navigate(appRoutes.main);
+    if (!isAuthenticated && location.pathname !== appRoutes.signup) {
+      navigate(appRoutes.login);
     }
   }, [isAuthenticated, location.pathname, navigate]);
 };
